437be51afe18ff774b566b645d04e40113802e57
joel <joelaviles413@gmail.com>
2021-09-22 22:50:42 -0500

    Practica 2 terminada
    El mapa debera desplegar un marcador por click y una linea entre los ultimos dos marcadores cuando se de doble click. las lineas anteriores se borran y se conserva la ultima

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 var salle = L.marker([21.15223412617155, -101.7113883047542]).addTo(mymap);
 
 function onEachFeature(feature, layer) {
-    var popupContent = "Soy un " + feature.geometry.type + " en la salle";
+    var popupContent = "Soy un " + feature.geometry.type + " en lasalle";
 
     if (feature.properties && feature.properties.popupContent) {
         popupContent += feature.properties.popupContent;
@@ -34,3 +34,54 @@ L.geoJSON(lasalle, {
 var marker = L.marker([21.15223412617155, -101.7113883047542])
     .addTo(mymap)
     .bindPopup("Holi");
+
+// Practica 2
+var markers = [];
+var polyLine;
+var createdLine = false;
+
+// borrar marcadores repetidos
+function filterArray(inputArr) {
+    var found = {};
+    var out = inputArr.filter(function(element){
+        return found.hasOwnProperty(element)? false : (found[element]=true);
+    });
+    return out;
+}
+
+function createLine(pointOne, pointTwo) {
+    polyLine = L.polyline([pointOne,pointTwo], { color: 'black' }).addTo(mymap);
+}
+
+function onMapDoubleClick(e) {
+
+    if(createdLine){
+        polyLine.remove();
+        createdLine = false;
+    }
+
+    marker1 = L.marker(e.latlng)
+        .addTo(mymap)
+        .bindPopup("You clicked the map at " + e.latlng.toString());
+    markers.push(marker1._latlng);
+
+    var filteredMarkers = filterArray(markers);
+
+    var pointOne = filteredMarkers[filteredMarkers.length - 2];
+    var pointTwo = filteredMarkers[filteredMarkers.length - 1];
+
+    if(!createdLine){
+        createLine(pointOne,pointTwo);
+        createdLine = true;
+    }
+}
+
+function onMapClick(e) {
+    marker2 = L.marker(e.latlng)
+        .addTo(mymap)
+        .bindPopup("You clicked the map at " + e.latlng.toString());
+    markers.push(marker2._latlng);
+}
+
+mymap.on("click", onMapClick);
+mymap.on("dblclick", onMapDoubleClick);
