c47f87fef4cba6c048757deaf85a73078ff763ef
joel <joelaviles413@gmail.com>
2021-09-22 20:07:02 -0500

    GeoJson

--- js/index.js
+++ js/index.js
@@ -1,72 +1,36 @@
-var lat = document.getElementById('lat');
-var lng = document.getElementById('lng');
-var nombreLugar = document.getElementById('nombre')
-var btnCoords = document.getElementById('btnSubirCoords');
 
 var mymap = L.map("mapid").setView(
-    [21.01581464273601, -101.25292611589512],
-    15
+    [21.152364203854884, -101.71115227036523],
+    16
 );
 
-var lugares = [
-    {
-        "nombre": "Plaza Galereña",
-        "lat": 20.987905245349697, 
-        "lng": -101.28424742853517
-    },
-    {
-        "nombre": "Centro de guanajuato",
-        "lat": 21.01581464273601,
-        "lng": -101.25292611589512
-    },
-    {
-        "nombre": "Alaia guanajuato",
-        "lat": 20.97443943694084,
-        "lng": -101.279534597727
-    },
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(mymap);
 
-]
+var salle = L.marker([21.15223412617155, -101.7113883047542]).addTo(mymap);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}
-).addTo(mymap);
+function onEachFeature(feature, layer) {
+    var popupContent = "Soy un " + feature.geometry.type + " en la salle";
 
-// Cargar marcadores
-cargarMarcadores = () => {
-    lugares.forEach(lugar => {
-        L.marker([lugar.lat, lugar.lng]).addTo(mymap).bindPopup(lugar.nombre);
-    });
-}
-
-window.onload = cargarMarcadores();
-
-// Crear marcador nuevo
-btnCoords.addEventListener('click', () => {
-    lugar = {
-        nombre: nombreLugar.value,
-        lat: lat.value,
-        lng: lng.value
+    if (feature.properties && feature.properties.popupContent) {
+        popupContent += feature.properties.popupContent;
     }
-    nombreLugar.value = "";
-    lat.value = "";
-    lng.value = "";
-    lugares.push(lugar);
-    cargarMarcadores();
-});
 
-// Poligono
-L.polygon(lugares)
-    .addTo(mymap)
-    .bindPopup("Zona peligrosa");
+    layer.bindPopup(popupContent);
+}
 
-// Figura sobre marcadores
-var circulo = L.circle([21.01581464273601, -101.25292611589512], {
-    color: "red",
-    fillColor: "#f03",
-    fillOpacity: 0.5,
-    radius: 250,
-})
+L.geoJSON(lasalle, {
+    filter: function (feature, layer) {
+        if (feature.properties) {
+            return true;
+        }
+    },
+    onEachFeature: onEachFeature,
+}).addTo(mymap);
+
+var marker = L.marker([21.15223412617155, -101.7113883047542])
     .addTo(mymap)
-    .bindPopup("Centro de guanajuato");
+    .bindPopup("Holi");
